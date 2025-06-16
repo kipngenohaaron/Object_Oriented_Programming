6522e780a724cdcc674d2b905569bb2e0e0eaec7
kipngenohaaron <kipngenohaaron@gmail.com>
2025-06-16 10:36:06 +0300

    file added

---  SequentialControl.cpp
+++  SequentialControl.cpp
@@ -19,4 +19,24 @@ int main() {
     return 0;
 }
 
-// 🔹 2. Area and Perimeter of a Rectangle
+// 🔹 2. Area and Perimeter of a Rectangle  
+#include <iostream>
+using namespace std;
+int main() {
+    float length, width, area, perimeter;
+
+    cout << "Enter Length of Rectangle: ";
+    cin >> length;
+    cout << "Enter Width of Rectangle: ";
+    cin >> width;
+
+    // Area Formula: A = L * W
+    area = length * width;
+    // Perimeter Formula: P = 2 * (L + W)
+    perimeter = 2 * (length + width);
+
+    cout << "Area of Rectangle: " << area << endl;
+    cout << "Perimeter of Rectangle: " << perimeter << endl;
+
+    return 0;
+}
