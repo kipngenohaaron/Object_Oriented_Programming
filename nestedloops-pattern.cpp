1c298f998c288250719049c1e7b603488f9fc82a
kipngenohaaron <kipngenohaaron@gmail.com>
2025-06-16 10:15:20 +0300

    pattern modified

--- nestedloops-pattern.cpp
+++ nestedloops-pattern.cpp
@@ -214,3 +214,75 @@ int main() {
 
     return 0;
 }
+
+// 🦋 8. Butterfly Pattern
+*       *
+**     **
+***   ***
+**** ****
+*********
+**** ****
+***   ***
+**     **
+*       *
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    // Upper half
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) cout << "*";
+        for (int s = 1; s <= 2 * (n - i); s++) cout << " ";
+        for (int j = 1; j <= i; j++) cout << "*";
+        cout << endl;
+    }
+
+    // Lower half
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) cout << "*";
+        for (int s = 1; s <= 2 * (n - i); s++) cout << " ";
+        for (int j = 1; j <= i; j++) cout << "*";
+        cout << endl;
+    }
+
+    return 0;
+}
+  
+// ⌛ 9. Sandglass Star Pattern
+// This program prints a sandglass pattern using nested loops.
+*********
+ *******
+  *****
+   ***
+    *
+   ***
+  *****
+ *******
+*********
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the number of rows (half of sandglass): ";
+    cin >> n;
+
+    // Top half
+    for (int i = n; i >= 1; i--) {
+        for (int s = 1; s <= n - i; s++) cout << " ";
+        for (int j = 1; j <= 2 * i - 1; j++) cout << "*";
+        cout << endl;
+    }
+
+    // Bottom half
+    for (int i = 2; i <= n; i++) {
+        for (int s = 1; s <= n - i; s++) cout << " ";
+        for (int j = 1; j <= 2 * i - 1; j++) cout << "*";
+        cout << endl;
+    }
+
+    return 0;
+}
