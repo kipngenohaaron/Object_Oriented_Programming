b2c15d392ff2e6c7e56876a526ea0ccb4559adb2
kipngenohaaron <kipngenohaaron@gmail.com>
2025-06-16 10:07:47 +0300

    pattern added

--- nestedloops-pattern.cpp
+++ nestedloops-pattern.cpp
@@ -129,3 +129,27 @@ int main() {
   *****
    ***
     *
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of rows (half of diamond): ";
+    cin >> n;
+
+    // Top half
+    for (int i = 1; i <= n; i++) {
+        for (int s = 1; s <= n - i; s++) cout << " ";
+        for (int j = 1; j <= 2 * i - 1; j++) cout << "*";
+        cout << endl;
+    }
+
+    // Bottom half
+    for (int i = n - 1; i >= 1; i--) {
+        for (int s = 1; s <= n - i; s++) cout << " ";
+        for (int j = 1; j <= 2 * i - 1; j++) cout << "*";
+        cout << endl;
+    }
+
+    return 0;
+}
