f316d77904500ad3afb826c83be3a0369821dc72
kipngenohaaron <kipngenohaaron@gmail.com>
2025-06-16 11:00:39 +0300

    file added

---  SequentialControl.cpp
+++  SequentialControl.cpp
@@ -167,4 +167,21 @@ int main() {
 
     return 0;
 }
-// 🔹 9. Leap Year Check    
+// 🔹 9. Leap Year Check   
+#include <iostream>
+using namespace std;
+int main() {
+    int year;
+
+    cout << "Enter a year: ";
+    cin >> year;
+
+    // Check if the year is a leap year
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        cout << year << " is a leap year." << endl;
+    } else {
+        cout << year << " is not a leap year." << endl;
+    }
+
+    return 0;
+} 
